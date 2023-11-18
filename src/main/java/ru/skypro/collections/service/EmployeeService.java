b984646b52d9f30a95f3c44b4f4397aab6da55d4
Rabbim57 <rabbim1998@mail.ru>
2023-11-18 20:52:17 +0300

    Done

---
+++ src/main/java/ru/skypro/collections/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package ru.skypro.collections.service;
+
+import ru.skypro.collections.model.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    Employee add(String firstName, String lastName);
+
+    Employee remove(String firstName, String lastName);
+
+    Employee find(String firstName, String lastName) throws EnumConstantNotPresentException;
+
+    List<Employee> getAll();
+}
