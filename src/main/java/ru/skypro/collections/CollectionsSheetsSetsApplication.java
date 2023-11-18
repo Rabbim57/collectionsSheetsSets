c6b2f300ae095a541468b845b35a861f50f87cf4
Rabbim57 <rabbim1998@mail.ru>
2023-11-18 19:19:52 +0300

    Выполнение

---
+++ src/main/java/ru/skypro/collections/CollectionsSheetsSetsApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsSheetsSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsSheetsSetsApplication.class, args);
+	}
+
+}
